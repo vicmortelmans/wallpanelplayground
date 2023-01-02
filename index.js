11e2298a36aedc3a8d97d1c279bab9b679f898e7
Vic Mortelmans <vicmortelmans@gmail.com>
2023-01-02 14:39:02 +0100

    met websocket in de server

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express")
 const app = express()
 const port=3000
+const WebSocketServer = require('ws')
+const wss = new WebSocketServer.Server({port: 3001 })
 
+wss.on("connection", ws=> {
+    console.log("new client connected")
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`)
+    })
+    ws.on("close", () => {
+        console.log("the client has disconnected")
+    })
+    ws.onerror= () =>{
+        console.log("Some error occurred")
+    }
+})
+console.log("The Websocket server is runnign on port 3001")
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        title: "Homepage",
-        message: "Hallo, this is my world!"
-    });
+    console.log("hallo")
+    res.render("home");
 })
 
 app.use(express.static("public"))
